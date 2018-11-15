2152a8a2df767b0d5088c0d232b82c6dfa63a7a1
BT_1N3_26 <sli7236@bths.edu>
2018-11-15 11:48:34 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {3, 1, 3, 1, 3};
+        int[] arr2 = new int[] {3, 1, 3, 3};
+        int[] arr3 = new int[] {3, 4, 3, 3, 4};
+        System.out.println(haveThree(arr1);
+        System.out.println(haveThree(arr2);
+        System.out.println(haveThree(arr3);
+    }
+
+    public static boolean haveThree(int[] nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (nums[i] == 3)
+            {
+                count++;
+            }
+        }
+        if (count == 3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
