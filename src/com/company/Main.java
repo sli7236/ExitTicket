0c9b3f7861fd271bc3792f557d6b4e4077b686fa
BT_1N3_26 <sli7236@bths.edu>
2018-11-16 11:09:51 -0500

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
         int[] arr1 = new int[] {3, 1, 3, 1, 3};
         int[] arr2 = new int[] {3, 1, 3, 3};
         int[] arr3 = new int[] {3, 4, 3, 3, 4};
-        System.out.println(haveThree(arr1);
-        System.out.println(haveThree(arr2);
-        System.out.println(haveThree(arr3);
+        System.out.println(haveThree(arr1));
+        System.out.println(haveThree(arr2));
+        System.out.println(haveThree(arr3));
     }
 
     public static boolean haveThree(int[] nums)
@@ -16,9 +16,16 @@ public class Main {
         int count = 0;
         for (int i = 0; i < nums.length; i++)
         {
-            if (nums[i] == 3)
+            if (i != nums.length - 1 && nums[i] == 3 && nums[i+1] != 3)
             {
                 count++;
+            }
+            else
+            {
+                if (i == nums.length - 1 && nums[i] == 3 && nums[i-1] != 3)
+                {
+                    count++;
+                }
             }
         }
         if (count == 3)
